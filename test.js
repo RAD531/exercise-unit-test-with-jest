000679275a2dea9497f48f596f3f2de91f49f2a2
RAD531 <69318839+RAD531@users.noreply.github.com>
2023-07-24 20:12:57 +0000

    updated readme and removed redundant tests

--- test.js
+++ test.js
@@ -1,15 +1,6 @@
 // import the function sum from the app.js file
 const { sum } = require('./app.js');
 
-// start your first test
-test('adds 14 + 9 to equal 23', () => {
-    //inside the test we call our sum function with 2 numbers
-    let total = sum(14, 9);
-
-    // we expect the sum of those 2 numbers to be 23
-    expect(total).toBe(23);
-});
-
 test("One euro should be 1.206 dollars", function(){
     //import the function from app.js
     const { fromEuroToDollar } = require('./app.js')
